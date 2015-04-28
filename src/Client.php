3bbe944a2612e689cf020a7c0cdc290a517a06cb
Luciano Jr <luciano@lucianojr.com.br>
2015-04-28 13:01:39 -0300

    Add php code sniffer standards

--- src/Client.php
+++ src/Client.php
@@ -1,21 +1,20 @@
 <?php namespace App;
 /**
- * Created by PhpStorm.
- * User: luciano
- * Date: 28/04/15
- * Time: 09:42
+ * Class Client
+ * @package App
+ * @author Luciano Jr <luciano@lucianojr.com.br>
  */
-
-class Client {
-    private $creator;
+class Client
+{
+    private $_creator;
 
     /**
      * Client constructor.
      */
     public function __construct()
     {
-        $this->creator = new ConcreteCreator();
-        echo $this->creator->doFactory(new ProductCLI());
+        $this->_creator = new ConcreteCreator();
+        echo $this->_creator->doFactory(new ProductCLI());
     }
 
 }
